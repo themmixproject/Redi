556b2290b905c27068854767a119a96ae8f12aff
themmixproject <steven.devalk@gmail.com>
2020-07-19 23:29:25 +0200

    undefined "re" fix

--- js/preload.js
+++ js/preload.js
@@ -1,5 +1,8 @@
 function parseURLParams(e){var n,t,l,o,r=e.indexOf("?")+1,p=e.indexOf("#")+1||e.length+1,i=e.slice(r,p-1),s=i.replace(/\+/g," ").split("&"),a={};if(i!==e&&""!==i){for(n=0;n<s.length;n++)o=s[n].split("=",2),t=decodeURIComponent(o[0]),l=decodeURIComponent(o[1]),a.hasOwnProperty(t)||(a[t]=[]),a[t].push(2===o.length?l:null);return a}}
-var urlParams = parseURLParams(window.location.href).re[0].split("/");
-if(urlParams!==undefined && urlParams[1]=="v") {
-    location.href = "https://www.youtube.com/watch?v="+urlParams[2];
+if(parseURLParams(window.location.href)){
+    var urlParams = parseURLParams(window.location.href).re[0].split("/");
+    if(urlParams[1]=="v") {
+        location.href = "https://www.youtube.com/watch?v="+urlParams[2];
+    }
+    
 }
